6999d884eae783a1fa715938084fa1a3f8944de2
Michal Rusina <michalrusina@gmail.com>
2020-03-19 13:57:50 +0100

    renaming defines

--- dn-remp-crm-auth.php
+++ dn-remp-crm-auth.php
@@ -4,7 +4,7 @@
 /**
  * Plugin Name: DN REMP CRM Auth
  * Plugin URI:  https://remp2020.com
- * Description: REMP CRM login, authentification and user data retrieval functions. You need to define DN_REMP_HOST in your wp-config.php file for this plugin to work correctly and then use included functions in your theme.
+ * Description: REMP CRM login, authentification and user data retrieval functions. You need to define <code>DN_REMP_CRM_HOST</code> in your <code>wp-config.php</code> file for this plugin to work correctly and then use included functions in your theme.
  * Version:     1.0.0
  * Author:      Michal Rusina
  * Author URI:  http://michalrusina.sk/
@@ -34,7 +34,7 @@ add_action( 'wp_enqueue_scripts', 'remp_login_form_script' );
 function remp_login_form( $echo = true ) {
 	$html = '';
 
-	if ( defined( 'DN_REMP_HOST' ) ) {
+	if ( defined( 'DN_REMP_CRM_HOST' ) ) {
 		$html = sprintf(
 			'
 			<form class="remp_login_form" action="%s">
@@ -43,7 +43,7 @@ function remp_login_form( $echo = true ) {
 				<button class="remp_login_submit" type="submit">%s</button>
 			</form>
 			',
-			DN_REMP_HOST . '/api/v1/users/login/',
+			DN_REMP_CRM_HOST . '/api/v1/users/login/',
 			__( 'E-mail', 'dn-remp-crm-auth' ),
 			__( 'Password', 'dn-remp-crm-auth' ),
 			__( 'Login', 'dn-remp-crm-auth' )
@@ -86,7 +86,7 @@ function remp_get_user( string $data = 'info' ) {
 		'subscriptions' => '/api/v1/users/subscriptions'
 	];
 
-	if ( !defined( 'DN_REMP_HOST' ) || !in_array( $data, array_keys( $apis ) ) ) {
+	if ( !defined( 'DN_REMP_CRM_HOST' ) || !in_array( $data, array_keys( $apis ) ) ) {
 		return null;
 	}
 
@@ -101,7 +101,7 @@ function remp_get_user( string $data = 'info' ) {
 		'Authorization' => 'Bearer ' . $token
 	];
 
-	$response = wp_remote_get( DN_REMP_HOST . $apis[ $data ], [ 'headers' => $headers ] );
+	$response = wp_remote_get( DN_REMP_CRM_HOST . $apis[ $data ], [ 'headers' => $headers ] );
 
 	if ( is_wp_error( $response ) ) {
 		error_log( 'REMP get_user_subscriptions: ' . $response->get_error_message() );
@@ -139,7 +139,7 @@ function remp_get_user_token() {
 function remp_crm_auth_init() {
 	load_plugin_textdomain( 'dn-remp-crm-auth' );
 	
-	if ( current_user_can( 'activate_plugins' ) && !defined( 'DN_REMP_HOST' ) ) {
+	if ( current_user_can( 'activate_plugins' ) && !defined( 'DN_REMP_CRM_HOST' ) ) {
 		add_action( 'admin_init', 'remp_crm_auth_deactivate' );
 		add_action( 'admin_notices', 'remp_crm_auth_deactivate_notice' );
 		unset( $_GET[ 'activate' ] );
@@ -179,7 +179,7 @@ function remp_crm_auth_deactivate() {
 
 function remp_crm_auth_deactivate_notice() {
 	printf( '<div class="error"><p>%s</p></div>',
-		__( 'The plugin <strong>DN REMP CRM Auth</strong> requires <code>DN_REMP_HOST</code> constant to be defined in <code>wp-config.php</code>.', 'dn-remp-crm-auth' )
+		__( 'The plugin <strong>DN REMP CRM Auth</strong> requires <code>DN_REMP_CRM_HOST</code> constant to be defined in <code>wp-config.php</code>.', 'dn-remp-crm-auth' )
 	);
 }
 
